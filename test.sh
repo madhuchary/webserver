07fa5db46fa0aac9ba5fc24d412631f3a828f65d
Madhu Sudhan <madhuchary80@gmail.com>
2017-07-19 19:56:28 +0530

    modified ssh options

--- test.sh
+++ test.sh
@@ -4,7 +4,7 @@ if [ $? -eq 0 ]
 then
 	echo "success"
 	scp index.html ubuntu@35.167.52.179:/tmp
-	ssh ubuntu@35.167.52.179 sudo /bin/sh -c 'cp /tmp/index.html /var/www/html/index.html'
+	ssh ubuntu@35.167.52.179 sudo cp /tmp/index.html /var/www/html/index.html
 	exit 0
 else
 	echo "Please enter only CAPS"
